dcac796d3f32ba08773ed7f53ac0c7a97eedf4bd
Vimal Singh <52564767+vimalsingh126@users.noreply.github.com>
2023-03-05 20:12:51 +0530

    Create emergency.c

---
+++ emergency.c
@@ -0,0 +1,127 @@
+/*client*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+#define EMERGENCY_SIGNAL "EMERGENCY"
+
+int main(int argc, char const *argv[]) {
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+    char message[1024] = {0};
+
+    // Create a socket for the client
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+
+    // Set up the server address
+    memset(&serv_addr, '0', sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+
+    // Connect to the server
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+
+    // Send the emergency signal to the server
+    strncpy(message, EMERGENCY_SIGNAL, sizeof(message));
+    send(sock, message, strlen(message), 0);
+
+    printf("Emergency signal sent\n");
+
+    return 0;
+}
+
+
+/*server*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+#define EMERGENCY_SIGNAL "EMERGENCY"
+
+int main(int argc, char const *argv[]) {
+    int server_fd, new_socket, valread;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[1024] = {0};
+
+    // Create a socket for the server
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Set socket options
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+        perror("setsockopt failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Set address information
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    // Bind the socket to the address
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Listen for incoming connections
+    if (listen(server_fd, 3) < 0) {
+        perror("listen failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Accept incoming connections
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0) {
+        perror("accept failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Receive data from the client
+    valread = read(new_socket, buffer, 1024);
+    if (valread < 0) {
+        perror("read failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Check if the data is an emergency signal
+    if (strcmp(buffer, EMERGENCY_SIGNAL) == 0) {
+        printf("Emergency signal received!\n");
+
+        // TODO: Trigger emergency response
+    } else {
+        printf("Received data: %s\n", buffer);
+    }
+
+    // Close the socket
+    close(new_socket);
+    close(server_fd);
+
+    return 0;
+}
+
