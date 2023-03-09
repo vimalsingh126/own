2f4c96393ec7ddea2adb5606753b63e9516d14a2
Vimal Singh <52564767+vimalsingh126@users.noreply.github.com>
2023-03-09 20:21:30 +0530

    Create requirements.c

---
+++ requirements.c
@@ -0,0 +1,4 @@
+Installing:
+ libssh-devel               x86_64           0.9.6-3.el8             rhel-8-for-x86_64-appstream-rpms           439 k
+Installing dependencies:
+ cmake-filesystem           x86_64           3.20.2-4.el8            rhel-8-for-x86_64-appstream-rpms            45 k
