cd68cf4300f4e0f262708620395d27620be53485
Vimal Singh <52564767+vimalsingh126@users.noreply.github.com>
2023-03-09 20:26:27 +0530

    Update requirements.c

--- requirements.c
+++ requirements.c
@@ -2,3 +2,5 @@ Installing:
  libssh-devel               x86_64           0.9.6-3.el8             rhel-8-for-x86_64-appstream-rpms           439 k
 Installing dependencies:
  cmake-filesystem           x86_64           3.20.2-4.el8            rhel-8-for-x86_64-appstream-rpms            45 k
+ 
+ openssl-devel.x86_64  openssl-libs.x86_64   
