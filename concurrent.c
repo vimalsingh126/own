b899a8043cab1879da4b9f25862c14a3a9309096
Vimal Singh <52564767+vimalsingh126@users.noreply.github.com>
2023-03-26 23:35:09 +0530

    Create concurrent.c

---
+++ concurrent.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+void handle_client(int client_socket) {
+    char buffer[1024];
+    int n;
+    
+    while ((n = read(client_socket, buffer, 1024)) > 0) {
+        write(client_socket, buffer, n);
+    }
+    
+    close(client_socket);
+    exit(0);
+}
+
+int main() {
+    int server_socket, client_socket;
+    struct sockaddr_in server_address, client_address;
+    pid_t pid;
+    socklen_t address_size;
+    
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    memset(&server_address, 0, sizeof(server_address));
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = INADDR_ANY;
+    server_address.sin_port = htons(8888);
+    bind(server_socket, (struct sockaddr *)&server_address, sizeof(server_address));
+    listen(server_socket, 5);
+    
+    while (1) {
+        address_size = sizeof(client_address);
+        client_socket = accept(server_socket, (struct sockaddr *)&client_address, &address_size);
+        
+        pid = fork();
+        if (pid == 0) {
+            // Child process code
+            close(server_socket);
+            handle_client(client_socket);
+        } else if (pid > 0) {
+            // Parent process code
+            close(client_socket);
+        } else {
+            // Error handling code
+            printf("Fork failed\n");
+            exit(1);
+        }
+    }
+    
+    return 0;
+}
